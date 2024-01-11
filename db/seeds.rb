56aaa7536ae262ac4fbe54afe1d9e2643ead97ce
MomokoJane Kina <momokokina@gmail.com>
2024-01-11 20:11:10 +0900

    migrated seeds and added phone number

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Destroying all old restaurants'
+Restaurant.destroy_all
+
+puts 'Creating 10 new restaurants'
+10.times do
+  rest = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "#{rest.name} has been created"
+end
+
+puts 'Done!'
