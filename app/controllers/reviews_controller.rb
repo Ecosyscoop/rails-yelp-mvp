aa653819d056a6319c5be5612282e6701ae87f4e
MomokoJane Kina <momokokina@gmail.com>
2024-01-13 00:08:40 +0900

    working on review and add new restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  # create the review controller for creating a new review according to the restaurant id and saving it.
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id]) #check why this is the param
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant #what does this do??
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "restaurants/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+puts "creating 10 restaurants"
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Name.first_name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Faker::Category.category
+  )
+  puts "#{restaurant.name} created."
+end
+puts "Done!"
+
+# run rails c and check in the rails console to see what fake data has been generated
